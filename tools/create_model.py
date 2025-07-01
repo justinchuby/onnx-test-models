1c58128a868225a4df1de41f26ab998cfc528dad
Justin Chu <justinchuby@users.noreply.github.com>
2025-07-01 11:11:52 -0700

    Process model (#1)
    Signed-off-by: Justin Chu <justinchuby@users.noreply.github.com>

---
+++ tools/create_model.py
@@ -0,0 +1,93 @@
+import pathlib
+import onnx_ir as ir
+import onnx_ir.passes.common as common_passes
+import onnxscript.version_converter
+
+
+class InitializerToInputPass(ir.passes.InPlacePass):
+    def __init__(self, size_limit: int = 64):
+        """
+        Initialize the pass to convert initializers to inputs.
+
+        Args:
+            size_limit (int): The maximum size of an initializer to be converted.
+        """
+        super().__init__()
+        self.size_limit = size_limit
+
+    def call(self, model: ir.Model) -> ir.passes.PassResult:
+        """
+        Convert initializers in the model to inputs.
+
+        Args:
+            model (ir.Model): The ONNX model to process.
+        """
+        modified = False
+        for initializer in tuple(model.graph.initializers.values()):
+            if initializer.const_value.size <= self.size_limit:
+                continue
+            modified = True
+            initializer.const_value = None
+            model.graph.initializers.pop(initializer.name)
+            if not initializer.is_graph_input():
+                model.graph.inputs.append(initializer)
+        return ir.passes.PassResult(model, modified)
+
+
+def process_model(model: ir.Model, target_version: int | None) -> ir.Model:
+    passes = ir.passes.Sequential(
+        *(
+            [
+                onnxscript.version_converter.ConvertVersionPass(
+                    target_version, fallback=True
+                ),
+            ]
+            if target_version is not None
+            else []
+        ),
+        common_passes.LiftConstantsToInitializersPass(),
+        InitializerToInputPass(),
+    )
+    model = passes(model).model
+    model = ir.external_data.load_to_model(model)
+    return model
+
+
+def create_model(
+    model: ir.Model, out_path: str, target_version: int | None = None
+) -> None:
+    """
+    Create a processed ONNX model from the input path and save it to the output path.
+
+    Args:
+        in_path: Path to the input ONNX model.
+        out_path: Path to save the processed ONNX model.
+    """
+    model = process_model(model, target_version)
+    out_dir = pathlib.Path(out_path).parent
+    out_dir.mkdir(parents=True, exist_ok=True)
+    ir.save(model, out_path)
+    print(f"Processed model saved to {out_path}")
+
+
+def main():
+    """
+    Main function to handle command line arguments and process the model.
+
+    Args:
+        args: Command line arguments.
+    """
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Process an ONNX model.")
+    parser.add_argument("in_path", type=str, help="Path to the input ONNX model.")
+    parser.add_argument(
+        "out_path", type=str, help="Path to save the processed ONNX model."
+    )
+    parsed_args = parser.parse_args()
+
+    create_model(ir.load(parsed_args.in_path), parsed_args.out_path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tools/create_model_from_model_zoo.py
@@ -0,0 +1,85 @@
+# Copyright (c) ONNX Project Contributors
+# SPDX-License-Identifier: Apache-2.0
+"""Test IR roundtrip with ONNX model zoo.
+
+Usage:
+    python model_zoo_test.py --jobs 8
+"""
+
+from __future__ import annotations
+
+import argparse
+import multiprocessing.pool
+import os
+import pathlib
+import tempfile
+
+import create_model
+import onnx_ir as ir
+import tqdm
+from onnx import hub
+
+
+def create_one_model(model_info: hub.ModelInfo):
+    model_name = model_info.model
+    model_path = model_info.model_path
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # For parallel testing, this must be in a separate process because hub.set_dir
+        # is not thread-safe.
+        hub.set_dir(temp_dir)
+        model_proto = hub.load(model_name)
+        assert model_proto is not None
+    print(f"\n----Creating from: {model_name} ----")
+    model_file_name = model_path.split("/")[-1].split(".")[0]
+    out_path = (
+        pathlib.Path(__file__).parent.parent
+        / "models"
+        / "model_zoo"
+        / f"{model_file_name}.textproto"
+    )
+
+    try:
+        create_model.create_model(ir.from_proto(model_proto), os.fspath(out_path))
+    except Exception as e:
+        print(f"Failed to create model {model_name}: {e}")
+        return
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Create test models from ONNX model zoo."
+    )
+    parser.add_argument(
+        "-k",
+        type=str,
+        default=None,
+        help="Keyword to filter the models. Default is None.",
+    )
+    parser.add_argument(
+        "--jobs",
+        type=int,
+        default=1,
+        help="Number of parallel jobs to run. Default is 1.",
+    )
+    args = parser.parse_args()
+
+    model_list = hub.list_models()
+    if args.k:
+        # Filter the models by name
+        name = args.k.lower()
+        model_list = [model for model in model_list if name in model.model.lower()]
+
+    # run checker on each model
+    # Use multi-processing to speed up the testing process
+    with multiprocessing.pool.Pool(args.jobs) as pool:
+        list(
+            tqdm.tqdm(
+                pool.imap_unordered(create_one_model, model_list),
+                "Creating...",
+                total=len(model_list),
+            )
+        )
+
+
+if __name__ == "__main__":
+    main()
